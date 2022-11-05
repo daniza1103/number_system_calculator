a9d5829d7ea82654f3359fb8c709f29b04be78d9
daniza1103 <danredtu1103@mail.ru>
2022-11-05 18:15:14 +0300

    encoding fix

--- src/main/java/com/github/daniza1103/Calculator.java
+++ src/main/java/com/github/daniza1103/Calculator.java
@@ -2,66 +2,56 @@ package com.github.daniza1103;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class Calculator extends JFrame {
 
-    /* создадим окно программы, а также поля для ввода данных, обозначим кнопки и создадим переменную, которая
-     * в последующей работе кода будет обрабатываться в качестве введенного значения */
+    /* СЃРѕР·РґР°РґРёРј РѕРєРЅРѕ РїСЂРѕРіСЂР°РјРјС‹, Р° С‚Р°РєР¶Рµ РїРѕР»СЏ РґР»СЏ РІРІРѕРґР° РґР°РЅРЅС‹С…, РѕР±РѕР·РЅР°С‡РёРј РєРЅРѕРїРєРё Рё СЃРѕР·РґР°РґРёРј РїРµСЂРµРјРµРЅРЅСѓСЋ, РєРѕС‚РѕСЂР°СЏ
+     * РІ РїРѕСЃР»РµРґСѓСЋС‰РµР№ СЂР°Р±РѕС‚Рµ РєРѕРґР° Р±СѓРґРµС‚ РѕР±СЂР°Р±Р°С‚С‹РІР°С‚СЊСЃСЏ РІ РєР°С‡РµСЃС‚РІРµ РІРІРµРґРµРЅРЅРѕРіРѕ Р·РЅР°С‡РµРЅРёСЏ */
 
     JTextArea display = new JTextArea();
     JPanel buttonPanel = new JPanel(new GridLayout(4, 3));
-    JButton button2 = new JButton("2-ая СС");
-    JButton button8 = new JButton("8-ая СС");
-    JButton button16 = new JButton("16-ая СС");
-    JButton buttonBack = new JButton("Удалить");
+    JButton button2 = new JButton("2-Р°СЏ РЎРЎ");
+    JButton button8 = new JButton("8-Р°СЏ РЎРЎ");
+    JButton button16 = new JButton("16-Р°СЏ РЎРЎ");
+    JButton buttonBack = new JButton("РЈРґР°Р»РёС‚СЊ");
     int value;
 
     Calculator() {
 
-        /* обозначим название программы и размер окна */
+        /* РѕР±РѕР·РЅР°С‡РёРј РЅР°Р·РІР°РЅРёРµ РїСЂРѕРіСЂР°РјРјС‹ Рё СЂР°Р·РјРµСЂ РѕРєРЅР° */
 
-        super("Калькулятор систем счисления");
+        super("РљР°Р»СЊРєСѓР»СЏС‚РѕСЂ СЃРёСЃС‚РµРј СЃС‡РёСЃР»РµРЅРёСЏ");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setResizable(false);
         setBounds(300, 300, 400, 200);
 
-        /* привяжем кнопкам определенные действия при помощи следующих методов */
+        /* РїСЂРёРІСЏР¶РµРј РєРЅРѕРїРєР°Рј РѕРїСЂРµРґРµР»РµРЅРЅС‹Рµ РґРµР№СЃС‚РІРёСЏ РїСЂРё РїРѕРјРѕС‰Рё СЃР»РµРґСѓСЋС‰РёС… РјРµС‚РѕРґРѕРІ */
 
-        button2.addActionListener(new ActionListener() {      //2-ая СС
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                value = Integer.parseInt(display.getText());
-                display.setText(Integer.toBinaryString(value));
-            }
+        //2-Р°СЏ РЎРЎ
+        button2.addActionListener(e -> {
+            value = Integer.parseInt(display.getText());
+            display.setText(Integer.toBinaryString(value));
         });
 
-        button8.addActionListener(new ActionListener() {      //8-ая СС
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                value = Integer.parseInt(display.getText());
-                display.setText(Integer.toOctalString(value));
-            }
+        //8-Р°СЏ РЎРЎ
+        button8.addActionListener(e -> {
+            value = Integer.parseInt(display.getText());
+            display.setText(Integer.toOctalString(value));
         });
 
-        button16.addActionListener(new ActionListener() {     //16-ая СС
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                value = Integer.parseInt(display.getText());
-                display.setText(Integer.toHexString(value));
-            }
+        //16-Р°СЏ РЎРЎ
+        button16.addActionListener(e -> {
+            value = Integer.parseInt(display.getText());
+            display.setText(Integer.toHexString(value));
         });
 
-        buttonBack.addActionListener(new ActionListener() {   //для удаления данных
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String temp = display.getText();
-                display.setText(temp.substring(0, temp.length() - 1));
-            }
+        //РґР»СЏ СѓРґР°Р»РµРЅРёСЏ РґР°РЅРЅС‹С…
+        buttonBack.addActionListener(e -> {
+            String temp = display.getText();
+            display.setText(temp.substring(0, temp.length() - 1));
         });
 
-        /*добавим кнопки в окно */
+        /*РґРѕР±Р°РІРёРј РєРЅРѕРїРєРё РІ РѕРєРЅРѕ */
 
         setLayout(new BorderLayout());
         add(display, BorderLayout.NORTH);
